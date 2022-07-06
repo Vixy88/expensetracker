0380c77f062db12f0a6298b6132953beda1ec5b4
MJakobsen <michael@executiveoperations.co.uk>
2022-07-06 22:45:42 +0100

    Fixed the clickhandler function so that it can toggle between the title and description also styled the button on expenseitem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,27 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  const [title, setTitle] = useState(true);
 
   const clickHandler = () => {
-    setTitle(props.description);
+    if (title === false) {
+      setTitle(true);
+    } else {
+      setTitle(false);
+    }
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{title ? props.title : props.description}</h2>
+        <div>
+          <button onClick={clickHandler} className="expense-item__btn-toggle">
+            {title ? "＋ Show Description " : "＋ Show Title"}
+          </button>
+        </div>
         <p className="expense-item__price">£{props.amount}</p>
-      </div>
-      <div>
-        <button onClick={clickHandler}>Show Description</button>
       </div>
     </Card>
   );
