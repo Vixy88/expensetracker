088b42b5b96c0332ef5f66a9597176360bfcf001
MJakobsen <michael@executiveoperations.co.uk>
2022-10-26 16:28:05 +0100

    added new code for the filter to be able to sort on year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,23 +1,29 @@
 import Card from "../UI/Card";
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
   return (
-    <Card className="expenses">
-      {items.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            description={expense.description}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-    </Card>
+    <section>
+      <Card className="expenses">
+        <div>
+          <ExpensesFilter data={items} />
+        </div>
+        {items.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              description={expense.description}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+      </Card>
+    </section>
   );
 };
 
